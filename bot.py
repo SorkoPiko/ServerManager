e8e4eca8411abb010988d6376bdf5b3eeb5aba66
Orion Railean <sorkopiko@icloud.com>
2020-05-14 09:06:05 +1000

    updated bot code

--- bot.py
+++ bot.py
@@ -1,17 +1,10 @@
 import discord
-
-client = discord.Client()
+from discord.ext import commands
+ 
+client = commands.Bot(command_prefix = ',')
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('Bot is ready.')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-client.run('699422804294238248')
+client.run('Njk5NDIyODA0Mjk0MjM4MjQ4.Xrx8-A.WiCFhu2R-Me4XdZBaAn7vM-CPvQ')
