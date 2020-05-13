517448beea5d998859ccd297726a551b9a05a222
Orion Railean <sorkopiko@icloud.com>
2020-05-14 09:36:09 +1000

    added MORE code

--- bot.py
+++ bot.py
@@ -7,4 +7,12 @@ client = commands.Bot(command_prefix = ',')
 async def on_ready():
     print('Bot is ready.')
 
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined a server.')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left a server.')
+
 client.run('Njk5NDIyODA0Mjk0MjM4MjQ4.Xrx8-A.WiCFhu2R-Me4XdZBaAn7vM-CPvQ')
