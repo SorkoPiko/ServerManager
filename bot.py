97a0573a3f3b17e235713a4dff9e3e983dd38c27
Orion Railean <sorkopiko@icloud.com>
2020-05-14 15:57:46 +1000

    added clear command

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import time
  
 client = commands.Bot(command_prefix = ',')
 
@@ -7,12 +8,15 @@ client = commands.Bot(command_prefix = ',')
 async def on_ready():
     print('Bot is ready.')
 
-@client.event
-async def on_member_join(member):
-    print(f'{member} has joined a server.')
+@client.command
+async def help(ctx):
+    await ctx.send('List of availible commands:\n^\help')
 
-@client.event
-async def on_member_remove(member):
-    print(f'{member} has left a server.')
+@client.command
+async def clear(ctx, amount=99999999999999):
+    new_clear = amount-1
+    await ctx.channel.purge(limit=amount)
+    time.sleep(0.2)
+    await ctx.send(f'I have cleared {new_clear} messages.')
 
 client.run('Njk5NDIyODA0Mjk0MjM4MjQ4.Xrx8-A.WiCFhu2R-Me4XdZBaAn7vM-CPvQ')
