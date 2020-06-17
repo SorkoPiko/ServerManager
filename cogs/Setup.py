7152b4abaaa58c336cf9a4f1195f6a31335114f8
Orion Railean <sorkopiko@icloud.com>
2020-06-17 16:02:30 +1000

    add setuppy

---
+++ cogs/Setup.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+class Setup(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+    
+    #Events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Bot is online.')
+
+    #Commands
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f':ping_pong: Pong! {round(self.client.latency * 1000)}ms :ping_pong:')
+
+        return
+    
+def setup(client):
+    client.add_cog(Setup(client))
